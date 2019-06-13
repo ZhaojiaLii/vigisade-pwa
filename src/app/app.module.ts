d35a9bdfe8c519f72fd5c0a6df091238c8e3df35
Guillaume Marceteau <guillaumemarceteau+brocelia@gmail.com>
2019-06-13 14:46:04 +0200

    Fix linting error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatButtonModule, MatIconModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
@@ -10,22 +11,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './containers/app.component';
 import { environment } from '../environments/environment';
 import { SharedModule } from './components/shared/shared.module';
-import { metaReducers, reducers } from "./store/app.reducer";
-import { AppEffects } from "./store/app.effects";
+import { metaReducers, reducers } from './store/app.reducer';
+import { AppEffects } from './store/app.effects';
 import { LoginComponent } from './components/login/containers/login.component';
-import {MatButtonModule, MatIconModule, MatListModule, MatToolbarModule} from '@angular/material';
 import { TutorialComponent } from './components/tutorial/containers/tutorial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TutorialComponent
+    TutorialComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({
       logOnly: environment.production,
       maxAge: 15,
--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,8 +1,8 @@
-import { ActionReducerMap, MetaReducer } from "@ngrx/store";
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 
-import { environment } from "../../environments/environment";
-import { State } from "./app.state";
-import { storeFreeze } from "ngrx-store-freeze";
+import { environment } from '../../environments/environment';
+import { State } from './app.state';
+import { storeFreeze } from 'ngrx-store-freeze';
 
 export const reducers: ActionReducerMap<State> = {};
 
