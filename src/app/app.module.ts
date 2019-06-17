4a957ad610247e732b77cd063be8015997c4506e
bgib <gibelot@brocelia.fr>
2019-06-17 09:46:33 +0200

    organisation SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatListModule, MatSelectModule, MatToolbarModule } from '@angular/material';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule} from 'ngx-bootstrap/modal';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -20,6 +22,7 @@ import { HomepageComponent } from './components/homepage/containers/homepage.com
 import { MenuComponent } from './components/menu/containers/menu.component';
 import { ProfileComponent } from './components/profile/containers/profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -43,6 +46,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     EffectsModule.forRoot([AppEffects]),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     SharedModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     MatButtonModule,
     MatToolbarModule,
     MatListModule,
