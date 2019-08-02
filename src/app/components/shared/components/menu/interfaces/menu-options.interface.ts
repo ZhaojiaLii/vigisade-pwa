66f690a7bd3db042a2bb877b23db096c96a20e66
Guillaume Marceteau <guillaumemarceteau+brocelia@gmail.com>
2019-08-02 11:17:33 +0200

    RM #90602 Update menu for the dangerous situation form

--- src/app/components/shared/components/menu/interfaces/menu-options.interface.ts
+++ src/app/components/shared/components/menu/interfaces/menu-options.interface.ts
@@ -11,4 +11,5 @@ export const DEFAULT_MENU_OPTIONS: MenuOptions = {
 export const MENU_SETUP: {[key: string]: MenuOptions} = {
   '/visit': {...DEFAULT_MENU_OPTIONS, routeLabel: 'Visite'},
   '/tutorial': {...DEFAULT_MENU_OPTIONS, displayBackNav: true, routeLabel: 'Tutorial'},
+  '/dangerous': {...DEFAULT_MENU_OPTIONS, displayBackNav: true, routeLabel: 'Situation dangereuse'}
 };
