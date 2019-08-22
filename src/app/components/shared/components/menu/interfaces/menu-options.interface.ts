1b150b38cc781ac30b553a51d3d609f3f12a0e80
zhli <li@brocelia.fr>
2019-08-22 17:19:25 +0200

    bug corrigé

--- src/app/components/shared/components/menu/interfaces/menu-options.interface.ts
+++ src/app/components/shared/components/menu/interfaces/menu-options.interface.ts
@@ -14,7 +14,7 @@ export const MENU_SETUP: {[key: string]: MenuOptions} = {
   dangerous: {...DEFAULT_MENU_OPTIONS, displayBackNav: true, routeLabel: 'Menu.Situation dangereuse'},
   history: {...DEFAULT_MENU_OPTIONS, displayBackNav: false, routeLabel: 'Menu.Historique des visites'},
   'history/:id': {...DEFAULT_MENU_OPTIONS, displayBackNav: true, routeLabel: 'Visite.Visite'},
-  profile: {...DEFAULT_MENU_OPTIONS, displayBackNav: true, routeLabel: 'Menu.Mon profil'},
+  profile: {...DEFAULT_MENU_OPTIONS, displayBackNav: false, routeLabel: 'Menu.Mon profil'},
   atraiter: {...DEFAULT_MENU_OPTIONS, displayBackNav: false, routeLabel: 'Menu.Atraiter'},
   'atraiter/:id': {...DEFAULT_MENU_OPTIONS, displayBackNav: true, routeLabel: 'Menu.Atraiter'},
 };
