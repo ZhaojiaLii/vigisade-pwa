91070f9e92324ff138bcca4bf8cb232d3b16a440
zhli <li@brocelia.fr>
2019-09-17 10:47:43 +0200

    RM 94326 hide content when selecting non-conforme

--- src/app/components/survey/components/survey-best-practice/survey-best-practice.component.ts
+++ src/app/components/survey/components/survey-best-practice/survey-best-practice.component.ts
@@ -16,6 +16,7 @@ export class SurveyBestPracticeComponent {
   showError = false;
   isCollapsed = true;
   imageloading = false;
+  hide = true;
 
   encode(event: any) {
     if (event.target.files && event.target.files[0]) {
@@ -31,12 +32,14 @@ export class SurveyBestPracticeComponent {
 
   updateValidators(required: boolean): void {
     if (required) {
+      this.hide = true;
       this.group.get('type').setValidators([Validators.required]);
       this.group.get('comment').setValidators([Validators.required, Validators.minLength(1)]);
       this.group.get('type').updateValueAndValidity();
       this.group.get('comment').updateValueAndValidity();
       this.group.get('photo').updateValueAndValidity();
     } else {
+      this.hide = false;
       this.group.get('type').clearValidators();
       this.group.get('comment').clearValidators();
       this.group.get('type').updateValueAndValidity();
