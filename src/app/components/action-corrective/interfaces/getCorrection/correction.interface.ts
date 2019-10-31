2821fc7b28c82092cf242e9e4adc347d562350d8
zhli <li@brocelia.fr>
2019-10-31 13:54:54 +0100

    RM 96169 FCT added dangerous_situation_image  and dangerous_situation_comment

--- src/app/components/action-corrective/interfaces/getCorrection/correction.interface.ts
+++ src/app/components/action-corrective/interfaces/getCorrection/correction.interface.ts
@@ -7,10 +7,12 @@ export interface Correction {
   resultUserfirstName: string;
   resultUserlastName: string;
   result_question_id: string;
-  type_dangerous_id: number;
   status: string;
   comment_question: string;
   image: string;
   user_id: number;
   date: string;
+  type_dangerous_id: number;
+  dangerous_situation_image: string;
+  dangerous_situation_comment: string;
 }
